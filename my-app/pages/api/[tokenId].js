903bbff90253f2b9e162fb6e59617b70d9424870
Narendiran Arthanarieswaran <narendiran.work@gmail.com>
2022-08-11 15:08:41 +0530

    Image link

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
   const tokenId = req.query.tokenId
   const name = `Crypto Dev #${tokenId}`;
   const description = 'CryptoDevs is an NFT Collection for Web3 Developers';
-  const image = "";
+  const image = `https://raw.githubusercontent.com/naren951/nft-collection/main/my-app/public/cryptodevs/${Number(tokenId)-1}.svg`;
   return res.json({
     name: name,
     description: description,
