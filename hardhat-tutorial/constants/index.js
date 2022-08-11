3825f30e70778f589694e08cb56594100acaf82d
Narendiran Arthanarieswaran <narendiran.work@gmail.com>
2022-08-11 15:44:33 +0530

    updated contract address

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,4 +1,4 @@
 const WHITELIST_CONTRACT_ADDRESS = "0x0D3A037CCf600d6fD61aaE67A69a2F32BA5C43dE";
-const METADATA_URL = "https://nft-collection-naren951.vercel.app/api/";
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
